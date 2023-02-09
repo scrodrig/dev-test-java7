1e3755cc97fa10a505c76ad9d022a5493a6e6abe
Schubert D. Rodriguez <schubert.rodriguez@outlook.com>
2023-02-09 11:22:37 -0500

    add production code and test for Event Storage Listener activities

--- src/main/java/com/persequor/broker/EventStorageListener.java
+++ src/main/java/com/persequor/broker/EventStorageListener.java
@@ -1,29 +1,75 @@
 package com.persequor.broker;
 
+import com.persequor.broker.exeptions.EventValidationErrorException;
+import com.persequor.exceptions.EventServiceErrorException;
 import com.persequor.model.Event;
-import com.persequor.repository.EventRepository;
+import com.persequor.model.EventList;
+import com.persequor.repository.IEventRepository;
+import com.persequor.repository.exceptions.EventRepositoryErrorException;
 
-public class EventStorageListener
-	implements EventListener
-{
-	private final EventRepository repository;
-	private EventQueue eventQueue;
+public class EventStorageListener implements IEventListener {
+	private final IEventRepository repository;
+	private IEventQueue eventQueue;
 
-	public EventStorageListener(
-			EventRepository repository
-			, EventQueue eventQueue
-	) {
+	public EventStorageListener(IEventRepository repository, IEventQueue eventQueue) {
 		this.repository = repository;
 		this.eventQueue = eventQueue;
 	}
 
 	@Override
-	public void handle(Event incomingEvent, int deliveryTag) {
-		//TODO: Validate that the incoming event is not an earlier event than the ones existing in the database (repository)
-		//      - As part of this, consider how you think the events should be sorted, and why.
-		//      - Handle validation errors the way you believe it should work
-		//TODO: Store event
-		//TODO: Pass event on to "statistics" queue and to "subscription" queue
-		//TODO: Acknowledge processed events
+	public void handle(Event incomingEvent, int deliveryTag) throws EventServiceErrorException {
+		// - As part of this, consider how you think the events should be sorted, and
+		// why
+		/*
+		 * Answer: EventList could be sorted in a decresent manner since could become
+		 * handy to have newest events available or assuming registration (as the
+		 * instant when are added to the system) could be useful have latest added
+		 * events into the system. However since for validation, I decided to use lambda
+		 * expressions (filter) is not mandatory to get an ordered list to begin with
+		 */
+
+		EventList eventList = repository.get(incomingEvent.getTrackedItemIds());
+		if (eventList.isEmpty()) {
+			// No validation needed
+			this.storeAndRegistration(incomingEvent, deliveryTag);
+			return;
+		}
+
+		if (isANewEvent(eventList, incomingEvent)) {
+			this.storeAndRegistration(incomingEvent, deliveryTag);
+			return;
+		}
+		// - Handle validation errors the way you believe it should work
+		throw new EventValidationErrorException();
+
 	}
+
+	private boolean isANewEvent(final EventList list, final Event incomingEvent) {
+		// TODO: Validate that the incoming event is not an earlier event than the ones
+		// existing in the database (repository)
+		return !list.stream().filter(e -> e.getEventTime().isAfter(incomingEvent.getEventTime())).findAny().isPresent();
+	}
+
+	private void storeAndRegistration(Event incomingEvent, int deliveryTag) throws EventRepositoryErrorException {
+		this.storeEventInRepository(incomingEvent);
+		this.passEventsToQueues(incomingEvent);
+		this.acknowledgeEventsWithTag(deliveryTag);
+	}
+
+	private void storeEventInRepository(Event incomingEvent) throws EventRepositoryErrorException {
+		// TODO: Store event
+		this.repository.persist(incomingEvent);
+	}
+
+	private void acknowledgeEventsWithTag(int deliveryTag) {
+		// TODO: Acknowledge processed events
+		this.eventQueue.acknowledge(deliveryTag);
+	}
+
+	private void passEventsToQueues(Event incomingEvent) {
+		// TODO: Pass event on to "statistics" queue and to "subscription" queue
+		this.eventQueue.push("statistics", incomingEvent);
+		this.eventQueue.push("subscription", incomingEvent);
+	}
+
 }
--- src/test/java/com/persequor/broker/EventStorageListenerTest.java
+++ src/test/java/com/persequor/broker/EventStorageListenerTest.java
@@ -1,24 +1,170 @@
 package com.persequor.broker;
 
-import com.persequor.repository.EventRepository;
+import com.persequor.broker.exeptions.EventValidationErrorException;
+import com.persequor.exceptions.EventServiceErrorException;
+import com.persequor.model.Event;
+import com.persequor.model.EventList;
+import com.persequor.repository.IEventRepository;
+import com.persequor.repository.exceptions.EventRepositoryErrorException;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.Matchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EventStorageListenerTest {
 	@Mock
-	private EventRepository repository;
+	private IEventRepository repository;
 	@Mock
-	private EventQueue eventQueue;
+	private IEventQueue eventQueue;
+	@Mock
+	private Event event;
+	@Mock
+	private Event event2;
 
+	// @InjectMocks
 	private EventStorageListener listener;
 
 	@Before
-	public void setup() {
+	public void setup() throws EventRepositoryErrorException {
 		MockitoAnnotations.initMocks(this);
-
+		when(repository.get(any())).thenReturn(new EventList());
 		listener = new EventStorageListener(repository, eventQueue);
 	}
 
 	/// TODO: Implement tests as needed
+
+	int deliveryTag = 32;
+
+	@Test
+	public void shouldVerifyPersistStorageOnRepository() throws EventServiceErrorException{
+		doNothing().when(repository).persist(event);
+		listener.handle(event, deliveryTag);
+		verify(repository).persist(event);
+	}
+
+	@Test
+	public void shouldVerifyAdditionToSubscription() throws EventServiceErrorException {
+		doNothing().when(eventQueue).push("subscription", event);
+		listener.handle(event, deliveryTag);
+		verify(eventQueue).push("subscription", event);
+	}
+
+	@Test
+	public void shouldVerifyAdditionToStatistics() throws EventServiceErrorException {
+		doNothing().when(eventQueue).push("statistics", event);
+		listener.handle(event, deliveryTag);
+		verify(eventQueue).push("statistics", event);
+	}
+
+	@Test
+	public void shouldVerifyNoOtherAdditionCanHappen() throws EventServiceErrorException {
+		doNothing().when(eventQueue).push("anyOtherStadistic", event);
+		listener.handle(event, deliveryTag);
+		verify(eventQueue, never()).push("anyOtherStadistic", event);
+	}
+
+	@Test
+	public void shouldNeverAcknoledgeEventsOnQueueByWrongDeliveryTag() throws EventServiceErrorException {
+		int wrongDeliveryTag = 33;
+		doNothing().when(eventQueue).acknowledge(deliveryTag);
+		listener.handle(event, wrongDeliveryTag);
+		verify(eventQueue, never()).acknowledge(deliveryTag);
+	}
+
+	@Test
+	public void shouldAcknoledgeEventsOnQueueByDeliveryTag() throws EventServiceErrorException {
+		doNothing().when(eventQueue).acknowledge(deliveryTag);
+		listener.handle(event, deliveryTag);
+		verify(eventQueue).acknowledge(deliveryTag);
+	}
+
+	@Test
+	public void shouldAcceptAnEventInAEmptyRepository() throws EventServiceErrorException {
+		when(repository.get(any())).thenReturn(new EventList());
+		listener.handle(event, deliveryTag);
+		verify(repository).persist(event);
+		verify(eventQueue).push("subscription", event);
+		verify(eventQueue).push("statistics", event);
+		verify(eventQueue).acknowledge(deliveryTag);
+	}
+
+	@Test
+	public void shouldAcceptAnEventAsANewEvent() throws EventServiceErrorException {
+		EventList eventList = new EventList();
+		Event olderEvent = new Event();
+		olderEvent.setEventTime(LocalDateTime.now().minusDays(1));
+		eventList.add(olderEvent);
+		when(event.getEventTime()).thenReturn(LocalDateTime.now());
+		when(repository.get(any())).thenReturn(eventList);
+		listener.handle(event, deliveryTag);
+		verify(repository).persist(event);
+		verify(eventQueue).push("subscription", event);
+		verify(eventQueue).push("statistics", event);
+		verify(eventQueue).acknowledge(deliveryTag);
+	}
+
+	@Test
+	public void shouldAcceptAnEventAsANewEventWithMoreThanOneEvent() throws EventServiceErrorException {
+		EventList eventList = new EventList();
+		Event olderEvent = new Event();
+		Event olderEventTwo = new Event();
+		olderEvent.setEventTime(LocalDateTime.now().minusDays(1));
+		olderEventTwo.setEventTime(LocalDateTime.now().minusDays(2));
+		eventList.add(olderEvent);
+		eventList.add(olderEventTwo);
+		when(event.getEventTime()).thenReturn(LocalDateTime.now());
+		when(repository.get(any())).thenReturn(eventList);
+		listener.handle(event, deliveryTag);
+		verify(repository).persist(event);
+		verify(eventQueue).push("subscription", event);
+		verify(eventQueue).push("statistics", event);
+		verify(eventQueue).acknowledge(deliveryTag);
+	}
+
+	@Test(expected=EventValidationErrorException.class)
+	public void shouldnotAcceptAnEventWhenTheEventIsOlder() throws EventServiceErrorException {
+		EventList eventList = new EventList();
+		Event newestEvent = new Event();
+		newestEvent.setEventTime(LocalDateTime.now());
+		eventList.add(newestEvent);
+		when(event.getEventTime()).thenReturn(LocalDateTime.now().minusDays(1));
+		when(repository.get(any())).thenReturn(eventList);
+		listener.handle(event, deliveryTag);
+		verify(repository, never()).persist(event);
+		verify(eventQueue, never()).push("subscription", event);
+		verify(eventQueue, never()).push("statistics", event);
+		verify(eventQueue, never()).acknowledge(deliveryTag);
+	}
+
+	@Test(expected=EventValidationErrorException.class)
+	public void shouldnotAcceptAnEventWhenTheEventIsOlderThanOneEventAndNewerThanOther()
+			throws EventServiceErrorException {
+		EventList eventList = new EventList();
+		Event newestEvent = new Event();
+		Event oldestEvent = new Event();
+		newestEvent.setEventTime(LocalDateTime.now());
+		oldestEvent.setEventTime(LocalDateTime.now().minusDays(2));
+		eventList.add(newestEvent);
+		eventList.add(oldestEvent);
+		when(event.getEventTime()).thenReturn(LocalDateTime.now().minusDays(1));
+		when(repository.get(any())).thenReturn(eventList);
+		listener.handle(event, deliveryTag);
+		verify(repository, never()).persist(event);
+		verify(eventQueue, never()).push("subscription", event);
+		verify(eventQueue, never()).push("statistics", event);
+		verify(eventQueue, never()).acknowledge(deliveryTag);
+	}
+
 }
