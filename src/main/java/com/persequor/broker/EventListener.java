8b7d1b7acfe34b3d078588b31a87b8701b2cacf8
Schubert D. Rodriguez <schubert.rodriguez@outlook.com>
2023-02-09 11:16:47 -0500

    remove renamed files

--- src/main/java/com/persequor/broker/EventListener.java
+++
@@ -1,7 +0,0 @@
-package com.persequor.broker;
-
-import com.persequor.model.Event;
-
-public interface EventListener {
-	void handle(Event event, int deliveryTag);
-}
--- src/main/java/com/persequor/broker/EventQueue.java
+++
@@ -1,9 +0,0 @@
-package com.persequor.broker;
-
-import com.persequor.model.Event;
-
-public interface EventQueue {
-	void push(String queue, Event event);
-	void acknowledge(int deliveryTag);
-	void addListener(String queue, EventListener listener);
-}
